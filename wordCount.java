f086ce6e2c2bc6d815c1fdb5abc6f8172f6dbeda
Harendra <112871832+827202@users.noreply.github.com>
2023-10-06 16:29:37 +0530

    Add files via upload

---
+++ wordCount.java
@@ -0,0 +1,11 @@
+
+public class wordCount {
+    public static void main(String[] args) {
+     
+        String word=" harendra is good boy";
+        
+        int countwords=word.split("\\s").length;
+        System.out.println(" count words is :" +countwords);
+    }
+    
+}
